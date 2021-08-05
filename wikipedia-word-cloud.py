6232b96ceae3ae6559da2af150a6629da7d90fa0
George A. McCarthy <65653499+georgeamccarthy@users.noreply.github.com>
2021-08-05 19:44:39 +0100

    internal: reformat with black

--- wikipedia-word-cloud.py
+++ wikipedia-word-cloud.py
@@ -1,9 +1,9 @@
- 
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 import wikipedia
 import streamlit as st
 import string
+
 
 def get_wordcloud(page_name):
     wiki_page = None
@@ -13,7 +13,7 @@ def get_wordcloud(page_name):
         wiki_page = wikipedia.page(e.options[0])
     except wikipedia.PageError as e:
         st.text(f"Page {page_name} does not match any pages. Try another.")
-    
+
     if wiki_page is None:
         return None, None, None
 
@@ -27,29 +27,24 @@ def get_wordcloud(page_name):
             wiki_text = wiki_text.replace(char, "")
     wiki_text = wiki_text.lower()
 
-    wiki_words = " ".join(wiki_text.split(" "))+""
-     
-    wordcloud = WordCloud(width = 800, height = 800).generate(wiki_words)
+    wiki_words = " ".join(wiki_text.split(" ")) + ""
+
+    wordcloud = WordCloud(width=800, height=800).generate(wiki_words)
     return wordcloud, wiki_title, wiki_url
+
 
 st.title("Wikipedia Word Cloud")
 
-query = st.text_input(
-    label="Enter the title of a Wikipedia article"
-)
+query = st.text_input(label="Enter the title of a Wikipedia article")
 
 if st.button(label="Search") or query:
     if query:
-        fig = plt.figure(figsize = (8, 8), facecolor = None)
+        fig = plt.figure(figsize=(8, 8), facecolor=None)
         plot = fig.add_subplot(111)
-        wordcloud, wiki_title, wiki_url  = get_wordcloud(query)
+        wordcloud, wiki_title, wiki_url = get_wordcloud(query)
 
         if wordcloud is not None:
             plot.axis("off")
             st.markdown(f"# [{wiki_title}]({wiki_url})")
             plot.imshow(wordcloud)
             st.pyplot(fig=fig)
-
-
-
- 
