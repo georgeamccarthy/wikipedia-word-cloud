77f81e41a64ebbb548910fe3db3a2fae786bb871
George A. McCarthy <65653499+georgeamccarthy@users.noreply.github.com>
2021-08-05 19:22:41 +0100

    feat: link to title

--- wikipedia-word-cloud.py
+++ wikipedia-word-cloud.py
@@ -15,10 +15,11 @@ def get_wordcloud(page_name):
         st.text(f"Page {page_name} does not match any pages. Try another.")
     
     if wiki_page is None:
-        return None, None
+        return None, None, None
 
     wiki_text = wiki_page.content
     wiki_title = wiki_page.title
+    wiki_url = wiki_page.url
 
     allowed_chars = string.ascii_letters + string.digits + " "
     for char in wiki_text:
@@ -29,7 +30,7 @@ def get_wordcloud(page_name):
     wiki_words = " ".join(wiki_text.split(" "))+""
      
     wordcloud = WordCloud(width = 800, height = 800).generate(wiki_words)
-    return wordcloud, wiki_title
+    return wordcloud, wiki_title, wiki_url
 
 st.title("Wikipedia Word Cloud")
 
@@ -41,11 +42,11 @@ if st.button(label="Search") or query:
     if query:
         fig = plt.figure(figsize = (8, 8), facecolor = None)
         plot = fig.add_subplot(111)
-        wordcloud, wiki_title = get_wordcloud(query)
+        wordcloud, wiki_title, wiki_url  = get_wordcloud(query)
 
         if wordcloud is not None:
             plot.axis("off")
-            st.title(wiki_title)
+            st.markdown(f"# [{wiki_title}]({wiki_url})")
             plot.imshow(wordcloud)
             st.pyplot(fig=fig)
 
